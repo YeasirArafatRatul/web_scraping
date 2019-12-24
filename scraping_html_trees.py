6585b0854956411b6571741cc23abbf21c29dc9a
arafat_ratul <yeasirarafat.ratul@gmail.com>
2019-12-24 13:58:09 +0600

    scraping_html_trees_regEx

--- scraping_html_trees.py
+++ scraping_html_trees.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.error import HTTPError, URLError
@@ -7,3 +8,17 @@ page = urlopen("http://www.pythonscraping.com/pages/page3.html")
 
 
 soupObj = BeautifulSoup(page, 'html.parser')
+
+title = soupObj.body.h1
+# gifts = soupObj.find_all
+
+gifts = soupObj.findAll(class_="gift")
+
+# print(title.get_text())
+# for gift in gifts:
+#     print(gift.get_text())
+
+expression = r'\$[0-9]*\,?[0-9]*?\.[0-9]+'
+
+prices = re.findall(expression, str(gifts))
+print(prices)
